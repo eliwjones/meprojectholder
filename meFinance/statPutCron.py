cede495f9475d876818ff6b194b6ed712e48b2d1
ELI <eli.jones@gmail.com>
2010-02-10 01:12:45 -0500

    added code to statPutCron.py to handle doDeltas during daily job

--- meFinance/statPutCron.py
+++ meFinance/statPutCron.py
@@ -4,6 +4,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api.labs import taskqueue
 from google.appengine.ext import db
+from google.appengine.api import memcache
 import meSchema
 
 
@@ -20,6 +21,10 @@ class putStats(webapp.RequestHandler):
             self.response.out.write('Put task!')
             delay = getStartDelay()
             taskAdd(delay,str(date.today()) + '-1',1,-1)
+        elif (cron == 'test'):
+            result = db.GqlQuery("Select * from stepDate Order By step desc").fetch(1)
+            step = result[0].step + 1
+            putEm(78,step)
             
     def post(self):
         count = int(self.request.get('counter'))
@@ -57,7 +62,11 @@ def putEm(count,step):
                                        step  = step,
                                        quote = quote)
                 meList.append(meStck)
-                
+
+    for stock in meList:
+        memcache.set("stck_" + stock.key().name(),stock)
+        doMeDeltas(stock.ID,step,step)
+        
     meStepDate = meSchema.stepDate(key_name = str(step),step = step, date = meDatetime)
     meList.append(meStepDate)
 
@@ -114,6 +123,75 @@ class meGDATA(object):
 
 application = webapp.WSGIApplication([('/cron/putStats',putStats)],
                                      debug = True)
+
+def doMeDeltas(stckID,startStep,stopStep):
+    count = 0
+    meDeltas = []
+    for i in range(startStep,stopStep+1):
+        meDelta = getDelta(stckID,i)
+        if meDelta is not None:
+            meDeltas.append(meDelta)
+            count += 1
+        if count == 100:
+            db.put(meDeltas)
+            meDeltas = []
+            count = 0
+    if count > 0:
+        db.put(meDeltas)
+
+def getDelta(stckID,currentStep):
+    currentKey = str(stckID) + "_" + str(currentStep)
+    keyList = []
+    for i in range(0,401):
+        keyStep = currentStep - i
+        if keyStep > 0:
+            key = str(stckID) + "_" + str(keyStep)
+            keyList.append(key)
+
+    results = memGetStcks_v2(keyList)
+    k=0
+    deltaList = []
+    
+    if results[0] is None:
+        return None
+    
+    lastQuote = results[0].quote
+    
+    for result in results:
+        if result is not None and float(result.quote) != 0.0 and float(lastQuote) != 0.0:
+            delta = (lastQuote-result.quote)/result.quote
+            floatString = str(delta)
+            if "-" in floatString:
+                floatString = floatString[:6]
+            else:
+                floatString = floatString[:5]
+            delta = float(floatString)
+            
+        else:
+            delta = 0.0
+        deltaList.append(delta)
+    from zlib import compress
+    compDelta = compress(str(deltaList),9)
+    meDelta = meSchema.delta(key_name = currentKey,cval = compDelta)
+    return meDelta
+
+def memGetStcks_v2(stckKeyList):
+    meList = []
+    memKeys = []
+    for stckKey in stckKeyList:
+        memKey = "stck_" + stckKey
+        memKeys.append(memKey)
+    memStocks = memcache.get_multi(memKeys)
+
+    for stckKey in stckKeyList:
+        memKey = "stck_" + stckKey
+        if memKey in memStocks:
+            meList.append(memStocks[memKey])
+        else:
+            stock = meSchema.stck.get_by_key_name(stckKey)
+            memcache.set(memKey,stock)
+            meList.append(stock)
+    return meList
 
 def taskAdd(delay,name,counter,step,wait=.5):
     try:
