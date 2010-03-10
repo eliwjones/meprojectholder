196b1cb302237249cf7f42831ba7e745b7dca5d6
ELI <eli.jones@gmail.com>
2010-03-09 23:37:35 -0500

    Stripped out verbose comments from proto-princeFunc.

--- meFinance/princeFunc.py
+++ meFinance/princeFunc.py
@@ -1,5 +1,3 @@
-#import meSchema
-
 
 def generatePositions():
     pos = {}
@@ -16,56 +14,52 @@ def generatePositions():
                     print cashdelta
                     print ''
     
+'''
+   Returns cash value indicating money locked up or released by given trade.
+   Must be modified to handle putting position changes to datastore.
 
-def processDesire(step):
-    print 'process desire!'
+   positions:
+       {'stck' : [shares,price,value]}
+
+       shares: -+ value depending on long/short.
+       price:  price when position was entered.
+       value:  shares*price for convenience.
+'''
 
 def mergePosition(desire,positions):
-    print 'Positions : %s ' % positions
-    print 'Desire    : %s ' % desire
     cash = 0
     for pos in desire:
         if pos in positions:
             signDes = cmp(desire[pos][0], 0)
             signPos = cmp(positions[pos][0], 0)
             if signDes != signPos:
-                print 'different position types.. release cash'
                 stockDiff = abs(positions[pos][0]) - abs(desire[pos][0])
                 priceDiff = positions[pos][1] - desire[pos][1]
                 if stockDiff >= 0:
                     cash  = abs(desire[pos][0])*positions[pos][1]
                     cash += desire[pos][0]*priceDiff
-                    print 'Submit Sell/Buy to Close Order'
                 else:
                     cash  = abs(positions[pos][0])*positions[pos][1]
                     cash += (-1)*positions[pos][0]*priceDiff
                     cash -= abs(stockDiff)*(desire[pos][1])
                     positions[pos][1] = desire[pos][1]
-                    print 'Submit Sell/Buy to Close Order'
-                    print 'Submit Buy/Sell to Open Order'
                 positions[pos][0] += desire[pos][0]
-                
                 if positions[pos][0] == 0:
                     del positions[pos]
                 else:
                     positions[pos][2] = positions[pos][0]*positions[pos][1]
             else:
-                print 'same position type.. combine positions return cash = -abs(desire[pos][2])'
                 cash = -abs(desire[pos][2])
                 positions[pos][0] += desire[pos][0]
                 positions[pos][2] += desire[pos][2]
-                positions[pos][1] = (positions[pos][2])/(positions[pos][0]) # Get new avg price
+                positions[pos][1] = (positions[pos][2])/(positions[pos][0])
         else:
-            print 'no existing postion, add position.. return cash = -abs(desire[pos][2])'
             cash = -abs(desire[pos][2])
             positions[pos] = [desire[pos][0],desire[pos][1],desire[pos][2]]
-
-    print positions
     return cash
 
 
 def main():
-    #processDesire(1)
     generatePositions()
 
 if __name__ == "__main__":
