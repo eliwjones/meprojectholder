8f893bb94a1cfdb4255969fab3135d3fca3d8601
ELI <eli.jones@gmail.com>
2010-09-26 01:35:34 -0400

    Rewrote code for calculateNtersects.py.  Now using something like three layers of deferred tasks.. but works fine.

--- meFinance/calculateNtersects.py
+++ meFinance/calculateNtersects.py
@@ -3,22 +3,62 @@
 import meSchema
 from google.appengine.ext import deferred
 from google.appengine.api.labs import taskqueue
+from google.appengine.api import memcache
+from pickle import dumps
+from google.appengine.ext import db
 
 def doDeferred(stopStep,startStep):
     deferred.defer(doNtersects, stopStep, startStep)
+
+def doNtersects(stopStep,startStep, cursor=None, i = 0):
+    from time import time
+    deadline = time() + 20.0
+    count = 400
+    while count == 400:
+        i += 1
+        query = meSchema.backTestResult.all().filter("stopStep =", stopStep).filter("startStep =", startStep).filter("percentReturn >",0.0)
+        if cursor is not None:
+            query.with_cursor(cursor)
+        if deadline > time():
+            backTests = query.fetch(400)
+            keyList = [alg.key().name().split('_')[0] for alg in backTests]
+            memKeyQuery = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", stopStep).filter("startStep =", startStep).filter("percentReturn >",0.0)
+            memKey = str(dumps(query).__hash__())
+            algKeys = memcache.get(memKey)
+            if algKeys is None:
+                memcache.set(memKey, keyList)
+            else:
+                algKeys.extend(keyList)
+                algKeys = list(set(algKeys))
+                memcache.set(memKey, algKeys)
+            count = len(backTests)
+            try:
+                deferred.defer(updateNs, stopStep, startStep, backTests, _name = "updateNs-" + str(stopStep) + "-" + str(startStep) + "-" + str(i))
+            except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
+                pass
+            cursor = query.cursor()
+        else:
+            try:
+                deferred.defer(doNtersects, stopStep, startStep, cursor, i, _name = "doNtersects-" + str(stopStep) + "-" + str(startStep) + "-" + str(i))
+                return
+            except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
+                pass
     
-def doNtersects(stopStep,startStep):
-    # Do greater than 0.0 N-tersects first.
-    backTests = meSchema.backTestResult.all().filter("stopStep =", stopStep).filter("startStep =", startStep).filter("percentReturn >",0.0)
-    backTests = backTests.fetch(2000)
+def updateNs(stopStep,startStep, backTests):
     N = [1,2]    # n=1 is one week back, which would give N = 2 since N=1 is 0 weeks back.
     Nkeys = {}
     for n in N:
         nStop = stopStep - n*400
         nStart = startStep - n*400
-        thisN = meSchema.backTestResult.all(keys_only=True).filter("stopStep =", nStop).filter("startStep =", nStart).filter("percentReturn >",0.0)
-        thisN = thisN.fetch(2000)
-        Nkeys[n] = [fullKey.name().split('_')[0] for fullKey in thisN]
+        thisN = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", nStop).filter("startStep =", nStart).filter("percentReturn >",0.0)
+        # Add in memcache check first before actual fetch().
+        memKey = str(dumps(thisN).__hash__())
+        algsN = memcache.get(memKey)
+        if algsN is None:
+            fullAlgKeys = thisN.fetch(4000)
+            algsN = [algkey.name().split('_')[0] for algkey in fullAlgKeys]
+            memcache.set(memKey, algsN)
+        Nkeys[n] = algsN
 
     for backTest in backTests:
         algKey = backTest.key().name().split('_')[0]
@@ -26,7 +66,15 @@ def doNtersects(stopStep,startStep):
         for n in N:
             if algKey in Nkeys[n]:
                 backTest.N = n+1
-    meSchema.batchPut(backTests)
+    # Fire off puts in deferred batches?
+    putList = []
+    for backTest in backTests:
+        putList.append(backTest)
+        if len(putList) > 100:
+            deferred.defer(db.put, putList)
+            putList = []
+    if putList > 0:
+        deferred.defer(db.put, putList)
     
     
     
--- meFinance/meUtilities.py
+++ meFinance/meUtilities.py
@@ -1,17 +1,39 @@
 # Utility file to contain code for deleting or converting entities
 import meSchema
 from google.appengine.ext import db
-    
+from google.appengine.ext import deferred
+from google.appengine.api.labs import taskqueue
+
+# Iterate through cursor and fire off deferred batches for deletes.
+# Call from remote_api console with command:
+#     deferred.defer(meUtilities.cleanupBackTestResult, 3555)
+
+def cleanupBackTestResult(maxStopStep, cursor = None):
+    count = 200
+    while count == 200:
+        query = meSchema.backTestResult.all(keys_only = True).filter("stopStep <", maxStopStep)
+        if cursor is not None:
+            query.with_cursor(cursor)
+        backTests = query.fetch(200)
+        count = len(backTests)
+        deferred.defer(deleteByKeyList, repr(backTests))
+        cursor = query.cursor()
+
+def deleteByKeyList(keylist):
+    from google.appengine.api import datastore_types
+    keylist = eval(keylist)
+    db.delete(keylist)
+
 def wipeoutDesires(cursor = None):
     total = 0
     count = 100
     while count == 100:
-        query = meSchema.desire.all()
+        query = meSchema.desire.all(keys_only = True)
         if cursor is not None:
             query.with_cursor(cursor)
         desire = query.fetch(100)
         count = len(desire)
-        db.delete(desire)
+        deferred.defer(deleteByKeyList, repr(desire))
         cursor = query.cursor()
         total += count
 
@@ -24,7 +46,7 @@ def wipeoutAlgStats(cursor = None):
             query.with_cursor(cursor)
         stats = query.fetch(100)
         count = len(stats)
-        db.delete(stats)
+        deferred.defer(deleteByKeyList, repr(stats))
         cursor = query.cursor()
         total += count
 
@@ -36,7 +58,7 @@ def wipeoutBackTests(cursor = None):
             query.with_cursor(cursor)
         btests = query.fetch(200)
         count = len(btests)
-        db.delete(btests)
+        deferred.defer(deleteByKeyList, repr(btests))
         cursor = query.cursor()
 
 def wipeoutDeltas(cursor = None):
@@ -47,5 +69,5 @@ def wipeoutDeltas(cursor = None):
             query.with_cursor(cursor)
         delts = query.fetch(100)
         count = len(delts)
-        db.delete(delts)
+        deferred.defer(deleteByKeyList, repr(delts))
         cursor = query.cursor()
