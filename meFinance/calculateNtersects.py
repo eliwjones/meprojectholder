90a47995f4e99f60334c1730bee2005d4b8ddd52
ELI <eli.jones@gmail.com>
2010-09-26 13:09:48 -0400

    Trimmed out some of the deferred calls in updateNs() and doNtersects().  Added parameters to feed in percentReturn filter so can do > 0.0 or < 0.0 calculations. Added doAll boolean to tell it to calculate all N properties less than a globalStop step.

--- meFinance/calculateNtersects.py
+++ meFinance/calculateNtersects.py
@@ -1,5 +1,3 @@
-# calculateNtersects.py
-# Used for calculation of N property on backTestResult Model.
 import meSchema
 from google.appengine.ext import deferred
 from google.appengine.api.labs import taskqueue
@@ -7,22 +5,23 @@ from google.appengine.api import memcache
 from pickle import dumps
 from google.appengine.ext import db
 
-def doDeferred(stopStep,startStep):
-    deferred.defer(doNtersects, stopStep, startStep)
+def doDeferred(stopStep,startStep, pReturnFilter = "percentReturn >", name = '', doAll = False):
+    deferred.defer(doNtersects, stopStep, startStep, pReturnFilter, name, 0, None, doAll)
 
-def doNtersects(stopStep,startStep, cursor=None, i = 0):
+def doNtersects(stopStep, startStep, pReturnFilter, name = '', i=0, cursor=None, doAll = False):
     from time import time
     deadline = time() + 20.0
-    count = 400
-    while count == 400:
+    globalStop = 15955
+    count = 200
+    while count == 200:
         i += 1
-        query = meSchema.backTestResult.all().filter("stopStep =", stopStep).filter("startStep =", startStep).filter("percentReturn >",0.0)
+        query = meSchema.backTestResult.all().filter("stopStep =", stopStep).filter("startStep =", startStep).filter(pReturnFilter,0.0).order("-percentReturn")
         if cursor is not None:
             query.with_cursor(cursor)
         if deadline > time():
-            backTests = query.fetch(400)
+            backTests = query.fetch(200)
             keyList = [alg.key().name().split('_')[0] for alg in backTests]
-            memKeyQuery = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", stopStep).filter("startStep =", startStep).filter("percentReturn >",0.0)
+            memKeyQuery = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", stopStep).filter("startStep =", startStep).filter(pReturnFilter,0.0).order("-percentReturn")
             memKey = str(dumps(query).__hash__())
             algKeys = memcache.get(memKey)
             if algKeys is None:
@@ -33,25 +32,33 @@ def doNtersects(stopStep,startStep, cursor=None, i = 0):
                 memcache.set(memKey, algKeys)
             count = len(backTests)
             try:
-                deferred.defer(updateNs, stopStep, startStep, backTests, _name = "updateNs-" + str(stopStep) + "-" + str(startStep) + "-" + str(i))
+                #deferred.defer(updateNs, stopStep, startStep, name, i, backTests, _name = "updateNs-" + str(stopStep) + "-" + str(startStep) + "-" + str(i) + '-' + name)
+                updateNs(stopStep, startStep, pReturnFilter, name, i, backTests)
             except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
                 pass
             cursor = query.cursor()
         else:
             try:
-                deferred.defer(doNtersects, stopStep, startStep, cursor, i, _name = "doNtersects-" + str(stopStep) + "-" + str(startStep) + "-" + str(i))
+                deferred.defer(doNtersects, stopStep, startStep, pReturnFilter, name, i, cursor, doAll, _name = "doNtersects-" + str(stopStep) + "-" + str(startStep) + "-" + str(i) + '-' + name)
                 return
             except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
                 pass
+    if (stopStep <= globalStop - 400) and doAll:
+        stopStep += 400
+        startStep += 400
+        try:
+            deferred.defer(doNtersects, stopStep, startStep, pReturnFilter, name, 0, None, doAll, _name = "doNtersects-" + str(stopStep) + "-" + str(startStep) + "-init-" + name)
+        except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
+            pass
+            
     
-def updateNs(stopStep,startStep, backTests):
-    N = [1,2]    # n=1 is one week back, which would give N = 2 since N=1 is 0 weeks back.
+def updateNs(stopStep,startStep, pReturnFilter, name, i, backTests):
+    N = [1,2]
     Nkeys = {}
     for n in N:
         nStop = stopStep - n*400
         nStart = startStep - n*400
-        thisN = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", nStop).filter("startStep =", nStart).filter("percentReturn >",0.0)
-        # Add in memcache check first before actual fetch().
+        thisN = meSchema.backTestResult.all(keys_only = True).filter("stopStep =", nStop).filter("startStep =", nStart).filter(pReturnFilter,0.0).order("-percentReturn")
         memKey = str(dumps(thisN).__hash__())
         algsN = memcache.get(memKey)
         if algsN is None:
@@ -63,18 +70,31 @@ def updateNs(stopStep,startStep, backTests):
     for backTest in backTests:
         algKey = backTest.key().name().split('_')[0]
         backTest.N = 1
-        for n in N:
-            if algKey in Nkeys[n]:
-                backTest.N = n+1
-    # Fire off puts in deferred batches?
+        if algKey in set(Nkeys[1]) - set(Nkeys[2]):
+            backTest.N = 2
+        elif algKey in set(Nkeys[1]) & set(Nkeys[2]):
+            backTest.N = 3
+
+    meSchema.batchPut(backTests)
+    '''
     putList = []
+    j = 0
     for backTest in backTests:
         putList.append(backTest)
         if len(putList) > 100:
-            deferred.defer(db.put, putList)
+            j += 1
+            try:
+                deferred.defer(db.put, putList, _name = "dbPut-" + str(stopStep) + "-" + str(startStep) + '-' + str(i) + '-' + str(j) + '-' + name)
+            except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
+                pass
             putList = []
     if putList > 0:
-        deferred.defer(db.put, putList)
+        j += 1
+        try:
+            deferred.defer(db.put, putList, _name = "dbPut-" + str(stopStep) + "-" + str(startStep) + '-' + str(i) + '-' + str(j) + '-' + name)
+        except (taskqueue.TaskAlreadyExistsError, taskqueue.TombstonedTaskError):
+            pass
+    '''
     
     
     
