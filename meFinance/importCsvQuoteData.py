5ba373ed24221ca29445ce1acd994de447da4e8d
eli <eli.jones@gmail.com>
2010-11-18 02:18:10 -0500

    Still trying to figure out how best to fill in the gaps between the High, Low values.... may just get lazy and lean on straight line fill since not sure how much value can be had from creating random walk.  In fact, may be best to set High and Low values to happen at about same step for each stock since correlation on daily basis is fairly high.  Right now, individual stock High values are set randomly so may not happen anywhere near the same moments.. which is somewhat unusual as far as general stock market behaviour is concerned.

--- meFinance/importCsvQuoteData.py
+++ meFinance/importCsvQuoteData.py
@@ -25,6 +25,7 @@ from google.appengine.api import memcache
 from datetime import datetime, date
 import meSchema
 import csv
+from random import random
 
 
 class CSVQuotes(webapp.RequestHandler):
@@ -43,20 +44,69 @@ class CSVQuotes(webapp.RequestHandler):
         # Index 1 is for step 1000081 to 1000160
         # Index N is for step N*80 + 1 to (N+1)*80  # plus 1000000
         putList = []
-        for N in len(dailyQuotes):
+        for N in range(len(dailyQuotes)):
             dailyOpenStep   = 1000000 + N*80 + 1
             dailyCloseStep  = 1000000 + N*80 + 80
-            OpenQuote = dailyQuotes[N][1]
-            CloseQuote = dailyQuotes[N][4]
             '''Must draw random steps for High and Low.'''
             daySteps = [step for step in range(dailyOpenStep+1,dailyCloseStep)]
-            randIndex = int(round(random()*len(daySteps)))
+            randIndex = int(round(random()*(len(daySteps)-1)))
             dailyHighStep = daySteps[randIndex]
             del daySteps[randIndex]
-            randIndex = int(round(random()*len(daySteps)))
+            randIndex = int(round(random()*(len(daySteps)-1)))
             dailyLowStep = daySteps[randIndex]
+            ''' Set Quote Values '''
+            OpenQuote = dailyQuotes[N][1]
+            HighQuote = dailyQuotes[N][2]
+            LowQuote = dailyQuotes[N][3]
+            CloseQuote = dailyQuotes[N][4]
+            ''' Create stck Entities for stckID Quotes'''
+            openStck = meSchema.stck(key_name=str(stckID) + '_' + str(dailyOpenStep),
+                                     ID = stckID, quote = float(OpenQuote), step = dailyOpenStep)
+            highStck = meSchema.stck(key_name=str(stckID) + '_' + str(dailyHighStep),
+                                     ID = stckID, quote = float(HighQuote), step = dailyHighStep)
+            lowStck  = meSchema.stck(key_name=str(stckID) + '_' + str(dailyLowStep),
+                                     ID = stckID, quote = float(LowQuote), step = dailyLowStep)
+            closeStck = meSchema.stck(key_name=str(stckID) + '_' + str(dailyCloseStep),
+                                     ID = stckID, quote = float(CloseQuote), step = dailyCloseStep)
+            putList.extend([openStck,highStck,lowStck,closeStck])
+            if len(putList) > 399:
+                db.put(putList)
+                putList = []
+        if len(putList) > 0:
+            db.put(putList)
+
+class fillRandomQuotes(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('I fill in the gaps between Open, High, Low, Close quotes.')
+        
+    def post(self):
+        ''' Starting at Step 1000001 and iterating over each group of 80,
+              Get all stck quotes for range and fill in gaps. '''
+        startStep = int(self.request.get('startStep'))
+        endStep = startStep + 79
+        stckQuotes = meSchema.stck.all().filter('step >=,' startStep).filter('step <=,' endStep).order('step').fetch(50)
+        ''' Should have 16 quotes.  4 for each stckID. '''
+        stckDict = {1:{}, 2:{}, 3:{}, 4:{}}
+        stepSeq = {1:[], 2:[], 3:[], 4:[]}
+        for stck in stckQuotes:
+            stckDict[stck.ID][stck.step] = stck.quote
+            stepSeq[stck.ID].append(stck.step)
+        ''' for stckID in stckDict, must fill in gaps between stepSeq values. '''
+
+def randomWalkBetweenPoints(startQuote, stopQuote, steps, minQuote, maxQuote):
+    straightLine = [((stopQuote - startQuote)/len(steps))*i for i in range(1,len(steps)+1)]
+    '''
+    for i in range(len(steps)):
+        # get random movement that is in appropriate range
+        # for straightLine[i] in min max range and closer to straightLine[-1]
+        # Can create 10 evenly spaced valueds between min max.
+        # Then multiply values by some factor due to distance from
+        # straightLine[i].  Closer to straightLine[i] gives higher factor.
+        pass
+    '''
             
-            
+        
 
 def taskAdd(name,startDayIndex,Symbol,wait=.5):
     try:
